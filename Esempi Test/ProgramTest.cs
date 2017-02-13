f9f6239ea6c785c33056141751847434a7ed5d92
Andrea Biancini <andrea.biancini@gmail.com>
2017-02-13 16:54:03 +0100

    Added parametric test

--- Esempi Test/ProgramTest.cs
+++ Esempi Test/ProgramTest.cs
@@ -20,5 +20,29 @@ namespace Esempi_Test
             // then
             Assert.AreEqual(12, sum);
         }
+
+        /// <summary>
+        /// Test parametrico per la somma di due numeri interi
+        /// </summary>
+        /// <param name="addend1">Primo addendo</param>
+        /// <param name="addend2">Secondo addendo</param>
+        /// <returns>Il risultato della somma</returns>
+        [TestCase(5, 7, ExpectedResult = 12)]
+        [TestCase(5, -7, ExpectedResult = -2)]
+        [TestCase(-5, 7, ExpectedResult = 2)]
+        [TestCase(5, 0, ExpectedResult = 5)]
+        [TestCase(0, 7, ExpectedResult = 7)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        public int TestSommaParametrico(int addend1, int addend2)
+        {
+            // given
+            Program program = new Program();
+
+            // when
+            int sum = program.add(addend1, addend2);
+
+            // then
+            return sum;
+        }
     }
 }
